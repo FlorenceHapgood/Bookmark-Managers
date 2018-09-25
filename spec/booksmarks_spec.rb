f15bf0d32c7d1c92577f740a9b990554bd64b786
Florence Hapgood <flohapgood@icloud.com>
2018-09-25 12:33:24 +0100

    Updated app to match my database' '

--- spec/booksmarks_spec.rb
+++ spec/booksmarks_spec.rb
@@ -5,8 +5,8 @@ describe Bookmark do
     it "returns all bookmarks" do
       bookmarks = Bookmark.all
       expect(bookmarks).to include("http://www.makersacademy.com")
-      expect(bookmarks).to include("http://askjeeves.com")
       expect(bookmarks).to include("http://www.google.com")
+      expect(bookmarks).to include("http://www.destroyallsoftware.com")
     end
   end
 end
--- spec/features/booksmarks_view_spec.rb
+++ spec/features/booksmarks_view_spec.rb
@@ -1,10 +1,8 @@
-
-
   feature "view bookmarks" do
     scenario "bookmark views" do
       visit "/bookmarks"
       expect(page).to have_content("http://www.makersacademy.com")
-      expect(page).to have_content("http://askjeeves.com")
+      expect(page).to have_content("http://www.destroyallsoftware.com")
       expect(page).to have_content("http://www.google.com")
     end
   end
