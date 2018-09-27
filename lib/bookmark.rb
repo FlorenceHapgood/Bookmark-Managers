ab1e613b85de8efb505edc712f6e9fed39e78ea1
Cameron Whitehead <camjw119@gmail.com>
2018-09-27 17:56:58 +0100

    Added DatabaseConnection class

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,46 +1,22 @@
-require "pg"
+require_relative "database_connection"
 
 class Bookmark
   def self.all
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-
-    result = connection.exec("SELECT * FROM bookmarks")
+    result = DatabaseConnection.query("SELECT * FROM bookmarks")
     result.map { |hash| { cocopops: hash["id"], url: hash["url"], title: hash["title"] } }
   end
 
   def self.create(url, title)
     return false unless is_url?(url)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-
-    connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}')")
+    DatabaseConnection.query("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}')")
   end
 
   def self.delete(id)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-
-    connection.exec("DELETE FROM bookmarks WHERE id = #{id};")
+    DatabaseConnection.query("DELETE FROM bookmarks WHERE id = #{id};")
   end
 
   def self.update(id, address, title)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-
-    connection.exec("UPDATE bookmarks SET url='#{address}', title='#{title}' WHERE id = #{id};")
+    DatabaseConnection.query("UPDATE bookmarks SET url='#{address}', title='#{title}' WHERE id = #{id};")
   end
 
   private
